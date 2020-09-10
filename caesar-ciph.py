ef663d34a64e665cf39163eb10f986e1bbba8826
Oscar Elhanafey <oscar.elhanafey@gmail.com>
2020-09-10 15:39:05 -0400

    caesar-ciph works with capitals and punctuation

--- caesar-ciph.py
+++ caesar-ciph.py
@@ -1,5 +1,6 @@
 from dict import open_dict
 import copy
+import string
 
 # given a ciphertext encoded with a caesar shift,
 # decrypt and return the plaintext
@@ -23,6 +24,12 @@ for shift in range(26):
                 text[i] = chr(ord('a')+overflow)
             else:
                 text[i] = chr(ord(text[i])+shift)
+        if text[i] >= 'A' and text[i] <= 'Z':
+            if ord(text[i])+shift > ord('Z'):
+                overflow = shift - (ord('Z') - ord(text[i]))-1
+                text[i] = chr(ord('A')+overflow)
+            else:
+                text[i] = chr(ord(text[i])+shift)
     shifted = ''.join(text)
     shifted_lst.append(shifted)
 
@@ -33,10 +40,13 @@ for i in range(len(shifted_lst)):
     words = shifted_lst[i].split(' ')
     count = 0
     for word in words:
+        # prune any punctuation from the word
+        word = word.translate(str.maketrans('', '', string.punctuation))
         if word in dict:
             count += 1
     if count > max:
         max = count
         max_ind = i
 
+# prints the caesar shift that contained the most real words
 print(shifted_lst[max_ind])
